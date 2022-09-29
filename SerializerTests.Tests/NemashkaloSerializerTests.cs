b323cfdd95b20c1b3532db08cb6906272b83198f
AlexandrNemashkalo <n.a.vi_talevich@mail.ru>
2022-09-29 03:14:17 +0300

    add serialiser v1

---
+++ SerializerTests.Tests/NemashkaloSerializerTests.cs
@@ -0,0 +1,229 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SerializerTests.Implementations;
+using SerializerTests.Interfaces;
+using SerializerTests.Nodes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializerTests.Tests;
+
+[TestClass]
+public class NemashkaloSerializerTests
+{
+    private  IListSerializer ListSerializer => new NemashkaloSerializer();
+
+    [TestMethod]
+    public async Task NemashkaloSerializerTest__ReturnSuccess()
+    {
+        Random rnd = new Random();
+        var head = CreateList(10000, (x) => $"qwerty{x}", (x) => rnd.Next(10000));
+        var ser = ListSerializer;
+
+        var deepCopyHead = await ser.DeepCopy(head);
+        var serHead = await SerializerAndDeserialize(head, ser);
+
+        AsserAreDataEqual(deepCopyHead, serHead);
+    }
+
+    [TestMethod]
+    public async Task DeserializerTest__InvalidStream__ReturnArgumentException()
+    {
+        await Assert.ThrowsExceptionAsync<ArgumentException>(() => ListSerializer.Deserialize(null));
+        using (var stream = new MemoryStream())
+        {
+            stream.Close();
+            await Assert.ThrowsExceptionAsync<ArgumentException>(() => ListSerializer.Deserialize(stream));
+        }
+    }
+
+    [DataRow("{}")]
+    [DataRow("[}")]
+    [DataRow("qwerty")]
+    [DataRow("[{\"Data\":\"qwerty\"}]")]
+    [DataRow("[{\"Id\":0,\"Data\":\"qwerty0\"}{\"Id\":1,\"Data\":\"qwerty1\"}]")]
+    [DataTestMethod]
+    public async Task DeserializerTest__InvalidData__ReturnArgumentException(string invalidData)
+    {
+        var bytes = Encoding.ASCII.GetBytes(invalidData);
+        using (var stream = new MemoryStream(bytes))
+        {
+            await Assert.ThrowsExceptionAsync<ArgumentException>(() => ListSerializer.Deserialize(stream));
+        }
+    }
+
+    [DataRow("[{\"Id\":0}]")]
+    [DataRow("[{\"Id\":0,\"Data\":\"qwerty\"}]")]
+    [DataRow("[{\"Id\":0,\"Data\":\"qwerty0\"},{\"Id\":1,\"Data\":\"qwerty1\"}]")]
+    [DataTestMethod]
+    public async Task DeserializerTest__CorrectData__ReturnSuccess(string correctData)
+    {
+        var bytes = Encoding.ASCII.GetBytes(correctData);
+        using (var stream = new MemoryStream(bytes))
+        {
+            var newHead = await ListSerializer.Deserialize(stream);
+            Assert.IsNotNull(newHead);
+        }
+    }
+
+    [TestMethod]
+    public async Task SerializerTest__NullNode__ReturnNullSuccess()
+    {
+        var newHead = await SerializerAndDeserialize(null);
+        Assert.IsNull(newHead);
+    }
+
+    [TestMethod]
+    public async Task SerializerTest__DifferentData_WithRandom__ReturnSuccess()
+    {
+        Random rnd = new Random();
+        var head = CreateList(10000, (x) => $"qwerty{x}", (x) => rnd.Next(10000));
+
+        var newHead = await SerializerAndDeserialize(head);
+
+        AsserAreDataEqual(head, newHead);
+    }
+
+    [TestMethod]
+    public async Task SerializerTest__DifferentData_WithoutRandom__ReturnSuccess()
+    {
+        var head = CreateList(10000, (x) => $"qwerty{x}", (x) => null);
+
+        var newHead = await SerializerAndDeserialize(head);
+
+        AsserAreDataEqual(head, newHead);
+    }
+
+    [TestMethod]
+    public async Task SerializerTest__EqualData_WithoutRandom__ReturnSuccess()
+    {
+        var head = CreateList(10000, (x) => $"qwerty", (x) => null);
+
+        var newHead = await SerializerAndDeserialize(head);
+
+        AsserAreDataEqual(head, newHead);
+    }
+
+    [TestMethod]
+    public async Task SerializerTest__NullData_WithoutRandom__ReturnSuccess()
+    {
+        var head = CreateList(10000, (x) => null, (x) => null);
+
+        var newHead = await SerializerAndDeserialize(head);
+
+        AsserAreDataEqual(head, newHead);
+    }
+
+    [TestMethod]
+    public async Task DeepCopyTest__DifferentData_WithRandom__ReturnSuccess()
+    {
+        Random rnd = new Random();
+        var head = CreateList(10000, (x) => $"qwerty{x}", (x) => rnd.Next(10000));
+
+        var newHead = await ListSerializer.DeepCopy(head);
+
+        AsserAreDataEqual(head, newHead);
+    }
+
+    [TestMethod]
+    public async Task DeepCopyTest__DifferentData_WithoutRandom__ReturnSuccess()
+    {
+        var head = CreateList(10000, (x) => $"qwerty{x}", (x) => null);
+
+        var newHead = await ListSerializer.DeepCopy(head);
+
+        AsserAreDataEqual(head, newHead);
+    }
+
+    [TestMethod]
+    public async Task DeepCopyTest__EqualData_WithoutRandom__ReturnSuccess()
+    {
+        var head = CreateList(10000, (x) => $"qwerty", (x) => null);
+
+        var newHead = await ListSerializer.DeepCopy(head);
+
+        AsserAreDataEqual(head, newHead);
+    }
+
+    [TestMethod]
+    public async Task DeepCopyTest__NullData_WithoutRandom__ReturnSuccess()
+    {
+        var head = CreateList(10000, (x) => null, (x) => null);
+
+        var newHead = await ListSerializer.DeepCopy(head);
+
+        AsserAreDataEqual(head, newHead);
+    }
+
+    private async Task<ListNode> SerializerAndDeserialize(ListNode oldHead, IListSerializer ser = null)
+    {
+        byte[] data;
+        ListNode newHead;
+        ser ??= ListSerializer;
+        using (var stream = new MemoryStream())
+        {
+            await ser.Serialize(oldHead, stream);
+            data = stream.ToArray();
+        }
+
+        using (var newStream = new MemoryStream(data))
+        {
+            return await ser.Deserialize(newStream);
+        }
+    }
+
+    private void AsserAreDataEqual(ListNode oldHead, ListNode newHead)
+    {
+        var currentNewNode = newHead;
+        var currentOldNode = oldHead;
+        while (currentNewNode != null && currentOldNode != null)
+        {
+            Assert.AreEqual(currentOldNode.Data, currentNewNode.Data, "currentNewNode.Data");
+            Assert.AreEqual(currentOldNode.Random?.Data, currentNewNode.Random?.Data, "currentNewNode.Random.Data");
+            Assert.AreEqual(currentOldNode.Next?.Data, currentNewNode.Next?.Data, "currentNewNode.Next.Data");
+            Assert.AreEqual(currentOldNode.Previous?.Data, currentNewNode.Previous?.Data, "currentNewNode.Previous.Data");
+
+            currentNewNode = currentNewNode.Next;
+            currentOldNode = currentOldNode.Next;
+        }
+        Assert.AreEqual(null, currentNewNode);
+        Assert.AreEqual(null, currentOldNode);
+    }
+
+    private ListNode CreateList(
+        int length,
+        Func<int,string> getData,
+        Func<int,int?> getRandom
+    )
+    {
+        var list = new List<ListNode>();
+        for (var i = 0; i < length; i++)
+        {
+            var listNode = new ListNode()
+            {
+                Data = getData(i)
+            };
+
+            if (i > 0)
+            {
+                listNode.Previous = list[i - 1];
+                list[i - 1].Next = listNode;
+            }
+
+            list.Add(listNode);
+        }
+
+        for (var i = 0; i < length; i++)
+        {
+            var randomIndex = getRandom(i);
+            if (randomIndex != null)
+            {
+                list[i].Random = list[randomIndex.Value];
+            }
+        }
+
+        return list[0];
+    }
+}
---
+++ SerializerTests/NemashkaloSerializer.cs
@@ -0,0 +1,183 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using SerializerTests.Interfaces;
+using SerializerTests.Nodes;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SerializerTests.Implementations;
+
+public class NemashkaloSerializer : IListSerializer
+{
+    class ListNodeComparer : IEqualityComparer<ListNode>
+    {
+        public bool Equals(ListNode? x, ListNode? y) => ReferenceEquals(x, y);
+
+        public int GetHashCode([DisallowNull] ListNode obj)
+            => HashCode.Combine(
+                obj.Data == null ? 0 : obj.Data.GetHashCode(),
+                obj.Random?.Data == null ? 0 : obj.Random?.Data.GetHashCode(),
+                obj.Previous?.Data == null ? 0 : obj.Previous.Data.GetHashCode(),
+                obj.Next?.Data == null ? 0 : obj.Next.Data.GetHashCode());
+    }
+
+    class ListNodeShort
+    {
+        public int Id { get; set; }
+        public int? RandomId { get; set; }
+        public string? Data { get; set; }
+    }
+
+    private readonly IEqualityComparer<ListNode> _listNodeComparer = new ListNodeComparer();
+
+    public Task<ListNode> DeepCopy(ListNode head)
+    {
+        var nodeDict = new Dictionary<ListNode, ListNode>(_listNodeComparer);
+        var randomNodeDict = new Dictionary<ListNode, ListNode>(_listNodeComparer);
+
+        var currentNode = head;
+        while (currentNode != null)
+        {
+            var newNode = new ListNode
+            {
+                Data = currentNode.Data
+            };
+
+            if(currentNode.Random != null)
+            {
+                if (nodeDict.TryGetValue(currentNode.Random, out var value))
+                {
+                    newNode.Random = value;
+                }
+                else
+                {
+                    randomNodeDict.Add(newNode, currentNode.Random);
+                }
+            }
+
+            if(currentNode != head)
+            {
+                newNode.Previous = nodeDict[currentNode.Previous];
+                newNode.Previous.Next = newNode;
+            }
+
+            nodeDict.Add(currentNode, newNode);
+            currentNode = currentNode.Next;
+        }
+
+        foreach(var (newNode, randomNode) in randomNodeDict)
+        {
+            newNode.Random = nodeDict[randomNode];
+        }
+
+        return Task.FromResult(nodeDict[head]);
+    }
+
+    public async Task<ListNode> Deserialize(Stream s)
+    {
+        if(s == null || !s.CanRead) throw new ArgumentException("Invalid stream for deserialization to ListNode");
+
+        var list = new List<ListNode>();
+        var randomNodeIdDict = new Dictionary<int,int>();
+
+        try
+        {
+            using (var reader = new StreamReader(s))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                var i = 0;
+                while (await jsonReader.ReadAsync())
+                {
+                    if (jsonReader.TokenType != JsonToken.StartObject) continue;
+
+                    var obj = await JObject.LoadAsync(jsonReader);
+                    var listNode = new ListNode
+                    {
+                        Data = obj.Value<string>("Data")
+                    };
+
+                    list.Add(listNode);
+
+                    var id = obj.Value<int?>("Id");
+                    if(id == null)
+                    {
+                        throw new ArgumentException();
+                    }
+                    var randomId = obj.Value<int?>("RandomId");
+
+                    if (randomId != null)
+                    {
+                        if (randomId > i)
+                        {
+                            randomNodeIdDict.Add(id.Value, randomId.Value);
+                        }
+                        else
+                        {
+                            listNode.Random = list[randomId.Value];
+                        }
+                    }
+
+                    if (i > 0)
+                    {
+                        list[i - 1].Next = listNode;
+                        listNode.Previous = list[i - 1];
+                    }
+
+                    i++;
+                }
+
+                if (i == 0) return null;
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new ArgumentException("Invalid data for deserialization to ListNode", ex);
+        }
+
+        foreach (var (id, randomId) in randomNodeIdDict)
+        {
+            list[id].Random = list[randomId];
+        }
+
+        return list[0];
+    }
+
+    public async Task Serialize(ListNode node, Stream stream)
+    {
+        if (stream == null || !stream.CanWrite) throw new ArgumentException("Invalid stream for serialization to ListNode");
+        var nodeDict = new Dictionary<ListNode, int>(_listNodeComparer);
+
+        var i = 0;
+        var currentNode = node;
+        while (currentNode != null)
+        {
+            nodeDict.Add(currentNode, i);
+            currentNode = currentNode.Next;
+            i++;
+        }
+
+        using (var writer = new StreamWriter(stream))
+        using (var jsonWriter = new JsonTextWriter(writer))
+        {
+            var ser = new JsonSerializer();
+            await jsonWriter.WriteStartArrayAsync();
+            foreach (var (listNode, id) in nodeDict)
+            {
+                var listNodeShort = new ListNodeShort()
+                {
+                    Id = id,
+                    Data = listNode.Data                    
+                };
+
+                if (listNode.Random != null)
+                {
+                    listNodeShort.RandomId = nodeDict[listNode.Random];
+                }
+                
+                ser.Serialize(jsonWriter, listNodeShort);
+            }
+
+            await jsonWriter.WriteEndArrayAsync();
+            await jsonWriter.FlushAsync();
+        }
+    }
+}
